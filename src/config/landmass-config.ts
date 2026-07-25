4a40bddc04dc04ac6ced8707f1c79f89816c9e78
Dmytro Karpliuk <d.karpljyk@gmail.com>
2026-07-25 07:45:36 +0700

    elevation profile dev viewer on chart.js

--- src/config/landmass-config.ts
+++ src/config/landmass-config.ts
@@ -1,9 +1,11 @@
+import type { ElevationProfile } from '../lib/elevation-profile-sampler';
 import { KM } from './common';
 import { ConfigService } from './config-service';
 
 export interface ContinentOptions {
   sizeMeters: number;
   amplitudeMeters: number;
+  elevationProfile: ElevationProfile;
 }
 
 export interface TerrainOptions {
@@ -27,6 +29,14 @@ export const landmassConfig = new ConfigService<LandmassConfig>({
     continents: {
       sizeMeters: 3000 * KM,
       amplitudeMeters: 12 * KM,
+      elevationProfile: [
+        [-1, -6 * KM],
+        [-0.3, -4 * KM],
+        [0, 0],
+        [0.15, 400],
+        [0.6, 1.5 * KM],
+        [1, 3 * KM],
+      ],
     },
   },
 });
